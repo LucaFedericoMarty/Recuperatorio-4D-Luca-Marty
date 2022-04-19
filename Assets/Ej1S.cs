35d976241e4fdd0f019cfe64e8760fb4dd672bd9
LucaFedericoMarty <101400526+LucaFedericoMarty@users.noreply.github.com>
2022-04-19 14:06:08 -0300

    EJ FINALIZADO

--- Assets/Ej1S.cs
+++ Assets/Ej1S.cs
@@ -12,29 +12,30 @@ public class Ej1S : MonoBehaviour
     {
         int preciokilo = 500;
         int kilo = 1000;
-        int 
+        int kilodivido = kilo / cantidadgramos;
+        int precioporgramo = preciokilo / kilodivido;
+        int descuentofurtiila = precioporgramo / 100;
+        int descuentofrutilla2 = descuentofurtiila * 10;
+
+        int descuentofinal = precioporgramo - descuentofrutilla2;
 
         if (gustodehelado == "DDL")
         {
-
+            Debug.Log("El precio final es: " + precioporgramo);
         }
 
         else if (gustodehelado == "CHO")
         {
-
+            Debug.Log("El precio final es: " + precioporgramo);
         }
 
         else if (gustodehelado == "FRU")
         {
+            Debug.Log("El precio final es: " + descuentofinal);
 
         }
 
         else if (cantidadgramos < 250 | cantidadgramos > 3000 )
-        {
-            Debug.Log("ERROR");
-        }
-
-        else
         {
             Debug.Log("ERROR");
         }
--- Assets/Ej2S.cs
+++ Assets/Ej2S.cs
@@ -4,10 +4,26 @@ using UnityEngine;
 
 public class Ej2S : MonoBehaviour
 {
+    public int edad;
+    public float altura;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (altura > 1.2 | edad > 13)
+        {
+            Debug.Log("INGRESO PERMITIDO");
+        }
+
+        else if (edad < 1 | altura < 0)
+        {
+            Debug.Log("ERROR");
+        }
+
+        else
+        {
+            Debug.Log("INGRESO DENEGADO");
+        }
     }
 
     // Update is called once per frame
