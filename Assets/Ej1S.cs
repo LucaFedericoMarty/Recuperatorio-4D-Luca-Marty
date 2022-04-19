b351272bebf5d07f5a8e3b8e5fd482939eae4da7
LucaFedericoMarty <101400526+LucaFedericoMarty@users.noreply.github.com>
2022-04-19 13:49:52 -0300

    Initial commit

---
+++ Assets/Ej1S.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ej1S : MonoBehaviour
+{
+    public int cantidadgramos;
+    public string gustodehelado;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int preciokilo = 500;
+        int kilo = 1000;
+        int 
+
+        if (gustodehelado == "DDL")
+        {
+
+        }
+
+        else if (gustodehelado == "CHO")
+        {
+
+        }
+
+        else if (gustodehelado == "FRU")
+        {
+
+        }
+
+        else if (cantidadgramos < 250 | cantidadgramos > 3000 )
+        {
+            Debug.Log("ERROR");
+        }
+
+        else
+        {
+            Debug.Log("ERROR");
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
